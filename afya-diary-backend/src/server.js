78a1622a2f60fb5b67e7547a8654f7c837057514
MwandikiAntony <antonymwandiki26@gmail.com>
2025-11-17 21:04:07 +0300

    made some changes to my server.js

--- afya-diary-backend/src/server.js
+++ afya-diary-backend/src/server.js
@@ -11,8 +11,3 @@ const PORT = process.env.PORT || 5000;
 require("./cron/reminderCron");
 
 // Connect DB then start server
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
-});
