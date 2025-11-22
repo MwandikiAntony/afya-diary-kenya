fc50bb7064f0a7ddc6141a829fb36f22921a1094
MwandikiAntony <antonymwandiki26@gmail.com>
2025-11-22 20:30:47 +0300

    updated app.js routes

--- afya-diary-frontend/src/App.js
+++ afya-diary-frontend/src/App.js
@@ -114,6 +114,7 @@ export default function App() {
   element= {<AddPatientPage/>} />,
 
       <Route path="/ai-helper" element={<AIChat />} />
+      <Route path="/ai-chat" element={<AIChat />} />
       <Route path="/mental-health/tips" element={<MentalHealthTips />} />
     <Route path="/mood-tracker" element={<MoodTracker />} />
 
